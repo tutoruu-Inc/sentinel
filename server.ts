544f4f480aa93c1820b70a35fbf10920b47eddff
Youssef <ya1027@nyu.edu>
2022-12-25 15:43:23 +0200

    setup standalone server

--- server.ts
+++ server.ts
@@ -1,29 +1,31 @@
 import { ApolloServer, BaseContext } from "@apollo/server";
-import fs from 'fs';
-import { mergeTypeDefs } from '@graphql-tools/merge';
+import fs from "fs";
+import { mergeTypeDefs } from "@graphql-tools/merge";
 import { Resolvers } from "./generated/types";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-import { expressMiddleware } from '@apollo/server/express4';
-import cors from 'cors';
-import express from 'express';
+import { CoreAPI } from "./services/core/index.js";
+import { queryable } from "./utils/Query.js";
 
-const resolvers: Resolvers = {};
+const resolvers: Resolvers = {
+  Query: {
+    user: async (parent, args, context, info) =>
+      await CoreAPI.getUser(args._id),
+    users: async (_, args) =>
+      await queryable(() => CoreAPI.getUsers(), args.input),
+  },
+};
 
-const typesArray = fs.readdirSync('./schema').map((file) => { 
-    return fs.readFileSync(`./schema/${file}`, 'utf8');
+const typesArray = fs.readdirSync("./schema").map((file) => {
+  return fs.readFileSync(`./schema/${file}`, "utf8");
 });
-const typeDefs = mergeTypeDefs(typesArray)
-
-const app = express();
+const typeDefs = mergeTypeDefs(typesArray);
 
 const server = new ApolloServer<BaseContext>({
   typeDefs,
   resolvers,
 });
-// Note you must call `server.start()` on the `ApolloServer`
-// instance before passing the instance to `expressMiddleware`
-await server.start();
 
-// Specify the path where we'd like to mount our server
-app.use('/graphql', cors<cors.CorsRequest>(), express.json(), expressMiddleware(server));
-
+await startStandaloneServer(server, {
+  listen: { port: 8000 },
+});
