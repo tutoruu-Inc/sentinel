4928aa93076433fbfc9d2020bae7f660893b6648
Youssef <ya1027@nyu.edu>
2022-12-27 14:58:09 +0200

    build script

---
+++ generators/build.js
@@ -0,0 +1,16 @@
+import { exec } from 'child_process';
+import util from 'util';
+
+const execute = util.promisify(exec);
+
+console.log('Building generators...');
+await execute('npx tsc -p ./config/tsconfig.json');
+console.log('Generating gateway...');
+await execute('node dist/generators/generateGateway');
+console.log('Generating types...');
+await execute('npm run codegen');
+console.log('Cleaning up...');
+await execute('npm run cleanup');
+console.log('Final build...');
+await execute('npx tsc -p ./config/tsconfig.json');
+console.log('Sentinel ready for deployment');
