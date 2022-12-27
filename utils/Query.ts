c0fcdafe1e4bfeae57a9569d25306bdf2aed1d09
Youssef <ya1027@nyu.edu>
2022-12-27 14:59:42 +0200

    comment out parser and query

--- utils/Query.ts
+++ utils/Query.ts
@@ -1,151 +1,150 @@
-import Fuse from "fuse.js";
-import { filterByOperation, parseJSON } from "./parser.js";
-import { QueryOptions } from "../generated/types";
+// import Fuse from "fuse.js";
+// import { filterByOperation, parseJSON } from "./parser.js";
+// import { QueryOptions } from "../generated/types";
 
-class Query<T> {
-  query: QueryOptions;
-  data: T[];
-  total: number;
+// class Query<T> {
+//   query: QueryOptions;
+//   data: T[];
+//   total: number;
 
-  constructor(query: QueryOptions = {}, data: T[] = [], total: number = 0) {
-    this.query = query;
-    this.data = data;
-    this.total = total || data.length;
-  }
+//   constructor(query: QueryOptions = {}, data: T[] = [], total: number = 0) {
+//     this.query = query;
+//     this.data = data;
+//     this.total = total || data.length;
+//   }
 
-  timebox() {
-    if (!this.query.timebox) return;
+//   timebox() {
+//     if (!this.query.timebox) return;
 
-    const {
-      created_after,
-      created_before,
-      updated_after,
-      updated_before,
-      create_prop,
-      update_prop,
-    } = this.query.timebox;
+//     const {
+//       created_after,
+//       created_before,
+//       updated_after,
+//       updated_before,
+//       create_prop,
+//       update_prop,
+//     } = this.query.timebox;
 
-      const created_date = (create_prop as keyof T) ?? "createdAt";
-      console.log({ length: this.data.filter(
-        (item) => new Date(item[created_date] as string) > new Date(created_after as string)
-      ).length})
-      if (created_after)
-        this.data = this.data.filter(
-          (item) => new Date(item[created_date] as string) > new Date(created_after)
-        );
+//       const created_date = (create_prop as keyof T) ?? "createdAt";
+//       console.log({ length: this.data.filter(
+//         (item) => new Date(item[created_date] as string) > new Date(created_after as string)
+//       ).length})
+//       if (created_after)
+//         this.data = this.data.filter(
+//           (item) => new Date(item[created_date] as string) > new Date(created_after)
+//         );
 
-      if (created_before)
-        this.data = this.data.filter(
-          (item) => new Date(item[created_date] as string) < new Date(created_before)
-        );
-    
+//       if (created_before)
+//         this.data = this.data.filter(
+//           (item) => new Date(item[created_date] as string) < new Date(created_before)
+//         );
 
-      const updated_date = (update_prop as keyof T) ?? "updatedAt";
-      if (updated_after)
-        this.data = this.data.filter(
-          (item) => new Date(item[updated_date] as string) > new Date(updated_after)
-        );
-      if (updated_before)
-        this.data = this.data.filter(
-          (item) => new Date(item[updated_date] as string) < new Date(updated_before)
-        );
-    
-  }
+//       const updated_date = (update_prop as keyof T) ?? "updatedAt";
+//       if (updated_after)
+//         this.data = this.data.filter(
+//           (item) => new Date(item[updated_date] as string) > new Date(updated_after)
+//         );
+//       if (updated_before)
+//         this.data = this.data.filter(
+//           (item) => new Date(item[updated_date] as string) < new Date(updated_before)
+//         );
 
-  filter() {
-    if (!this.query.filter) return;
+//   }
 
-    const { function: fn, value, prop } = this.query.filter;
+//   filter() {
+//     if (!this.query.filter) return;
 
-    this.data = filterByOperation<T>(fn, this.data, prop, value);
-  }
+//     const { function: fn, value, prop } = this.query.filter;
 
-  search() {
-    if (!this.query.search) return;
+//     this.data = filterByOperation<T>(fn, this.data, prop, value);
+//   }
 
-    const { value, paths, case_sensitive } = this.query.search;
+//   search() {
+//     if (!this.query.search) return;
 
-    const options = {
-      isCaseSensitive: !!case_sensitive,
-      includeScore: true,
-      includeMatches: true,
-      shouldSort: true,
-      keys: paths,
-    };
+//     const { value, paths, case_sensitive } = this.query.search;
 
-    const fuse = new Fuse(this.data, options);
-    this.data = fuse.search(value).map((result) => {
-      return {
-        ...result.item,
-        score: result.score,
-        matches: result.matches,
-      };
-    });
-  }
+//     const options = {
+//       isCaseSensitive: !!case_sensitive,
+//       includeScore: true,
+//       includeMatches: true,
+//       shouldSort: true,
+//       keys: paths,
+//     };
 
-  sort() {
-    if (!this.query.sort) return;
+//     const fuse = new Fuse(this.data, options);
+//     this.data = fuse.search(value).map((result) => {
+//       return {
+//         ...result.item,
+//         score: result.score,
+//         matches: result.matches,
+//       };
+//     });
+//   }
 
-    const { prop, direction, function: fn } = this.query.sort;
+//   sort() {
+//     if (!this.query.sort) return;
 
-    const compareProp = (a: T, b: T) => {
-      let first;
+//     const { prop, direction, function: fn } = this.query.sort;
 
-      let A, B;
-      A = a[prop as keyof T];
-      B = b[prop as keyof T];
+//     const compareProp = (a: T, b: T) => {
+//       let first;
 
-      if (!A || !B) return 0;
+//       let A, B;
+//       A = a[prop as keyof T];
+//       B = b[prop as keyof T];
 
-      switch (fn) {
-        case "locale":
-          if (typeof A !== "string" || typeof B !== "string") return 0;
-          first = A.localeCompare(B);
-          break;
-        default:
-          first = (A as number) - (B as number);
-          break;
-      }
+//       if (!A || !B) return 0;
 
-      if (direction === "desc") first *= -1;
-      return first;
-    };
+//       switch (fn) {
+//         case "locale":
+//           if (typeof A !== "string" || typeof B !== "string") return 0;
+//           first = A.localeCompare(B);
+//           break;
+//         default:
+//           first = (A as number) - (B as number);
+//           break;
+//       }
 
-    this.data = this.data.sort(compareProp);
-  }
+//       if (direction === "desc") first *= -1;
+//       return first;
+//     };
 
-  paginate() {
-    if (!this.query.paginate) return;
+//     this.data = this.data.sort(compareProp);
+//   }
 
-    let { limit, page } = this.query.paginate;
+//   paginate() {
+//     if (!this.query.paginate) return;
 
-    if (!limit) limit = this.data.length;
-    if (!page) page = 1;
+//     let { limit, page } = this.query.paginate;
 
-    const skip = (page - 1) * limit;
-    this.data = this.data.slice(skip, skip + limit);
-  }
+//     if (!limit) limit = this.data.length;
+//     if (!page) page = 1;
 
-  apply(query = this.query, data = this.data) {
-    this.query = query;
-    this.data = data;
-    this.timebox();
-    this.filter();
-    this.search();
-    this.sort();
-    this.paginate();
-    
-    return this;
-  }
-}
+//     const skip = (page - 1) * limit;
+//     this.data = this.data.slice(skip, skip + limit);
+//   }
 
-export async function queryable<T>(
-  callback: () => Promise<T[]>,
-  query: QueryOptions | null | undefined
-) {
-  const response = await callback();
-  if (!query) return response;
+//   apply(query = this.query, data = this.data) {
+//     this.query = query;
+//     this.data = data;
+//     this.timebox();
+//     this.filter();
+//     this.search();
+//     this.sort();
+//     this.paginate();
 
-  const q = new Query<T>(query, response);
-  return q.apply().data;
-}
+//     return this;
+//   }
+// }
+
+// export async function queryable<T>(
+//   callback: () => Promise<T[]>,
+//   query: QueryOptions | null | undefined
+// ) {
+//   const response = await callback();
+//   if (!query) return response;
+
+//   const q = new Query<T>(query, response);
+//   return q.apply().data;
+// }
--- utils/parser.ts
+++ utils/parser.ts
@@ -1,79 +1,79 @@
-import { FilterOperation } from "../generated/types";
+// import { FilterOperation } from "../generated/types";
 
-export const parseJSON = (str: string): string | object => {
-  try {
-    return JSON.parse(str);
-  } catch (e) {
-    return str;
-  }
-};
+// export const parseJSON = (str: string): string | object => {
+//   try {
+//     return JSON.parse(str);
+//   } catch (e) {
+//     return str;
+//   }
+// };
 
-export const serialize = (obj: object, prefix: string): string => {
-  let str = [],
-    p;
-  for (p in obj) {
-    if (obj.hasOwnProperty(p)) {
-      let k = prefix ? prefix + "[" + p + "]" : p,
-        v = obj[p as keyof typeof obj];
-      str.push(
-        v !== null && typeof v === "object"
-          ? serialize(v, k)
-          : encodeURIComponent(k) + "=" + encodeURIComponent(v)
-      );
-    }
-  }
-  return str.join("&");
-};
+// export const serialize = (obj: object, prefix: string): string => {
+//   let str = [],
+//     p;
+//   for (p in obj) {
+//     if (obj.hasOwnProperty(p)) {
+//       let k = prefix ? prefix + "[" + p + "]" : p,
+//         v = obj[p as keyof typeof obj];
+//       str.push(
+//         v !== null && typeof v === "object"
+//           ? serialize(v, k)
+//           : encodeURIComponent(k) + "=" + encodeURIComponent(v)
+//       );
+//     }
+//   }
+//   return str.join("&");
+// };
 
-export function filterByOperation<T>(
-  operation: FilterOperation,
-  data: T[],
-  key: string,
-  value: string
-) {
-  function assertOperation(
-    operation: FilterOperation,
-    data: T,
-    value: string
-  ): boolean {
-    let item;
+// export function filterByOperation<T>(
+//   operation: FilterOperation,
+//   data: T[],
+//   key: string,
+//   value: string
+// ) {
+//   function assertOperation(
+//     operation: FilterOperation,
+//     data: T,
+//     value: string
+//   ): boolean {
+//     let item;
 
-    const path = key.split(".");
-    if (path.length > 1) {
-      let iterator = data;
-      for (let i = 0; i < path.length; i++)
-        iterator = iterator[path[i] as keyof typeof item];
+//     const path = key.split(".");
+//     if (path.length > 1) {
+//       let iterator = data;
+//       for (let i = 0; i < path.length; i++)
+//         iterator = iterator[path[i] as keyof typeof item];
 
-      item = iterator;
-    } else item = data[key as keyof typeof data];
+//       item = iterator;
+//     } else item = data[key as keyof typeof data];
 
-    switch (operation) {
-      case "gt":
-        return item > value;
-      case "lt":
-        return item < value;
-      case "gte":
-        return item >= value;
-      case "lte":
-        return item >= value;
-      case "eq":
-        return item == value;
-      case "neq":
-        return item != value;
-      case "in":
-        return (
-          (!Array.isArray(item) && typeof item !== "string") ||
-          item.includes(value)
-        );
-      case "nin":
-        return (
-          (!Array.isArray(item) && typeof item !== "string") ||
-          !item.includes(value)
-        );
-      default:
-        return false;
-    }
-  }
+//     switch (operation) {
+//       case "gt":
+//         return item > value;
+//       case "lt":
+//         return item < value;
+//       case "gte":
+//         return item >= value;
+//       case "lte":
+//         return item >= value;
+//       case "eq":
+//         return item == value;
+//       case "neq":
+//         return item != value;
+//       case "in":
+//         return (
+//           (!Array.isArray(item) && typeof item !== "string") ||
+//           item.includes(value)
+//         );
+//       case "nin":
+//         return (
+//           (!Array.isArray(item) && typeof item !== "string") ||
+//           !item.includes(value)
+//         );
+//       default:
+//         return false;
+//     }
+//   }
 
-  return data.filter((item) => assertOperation(operation, item, value));
-}
+//   return data.filter((item) => assertOperation(operation, item, value));
+// }
