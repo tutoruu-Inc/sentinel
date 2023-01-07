9ceb54f783916980cbd7fc825220d521a3eb817d
Youssef <ya1027@nyu.edu>
2023-01-07 16:35:37 +0200

    add base query and mutation types

--- generators/generateGQLSchema.ts
+++ generators/generateGQLSchema.ts
@@ -4,25 +4,25 @@ import type {
   Query,
   FieldType,
   Object,
-  ScalarType,
-} from "./fetchSchema.js";
-import { getScalars } from "./generateScalars.js";
-import { writeSchema } from "./writer.js";
+  ScalarType
+} from './fetchSchema.js';
+import { getScalars } from './generateScalars.js';
+import { writeSchema } from './writer.js';
 
 export const parseField = (field: Field): string =>
   `${field.name}: ${
     field.scalarType?.name ?? field.fieldType?.name ?? field.baseType?.name
-  }${field.required ? "!" : ""}`;
+  }${field.required ? '!' : ''}`;
 
 export const parseFunction = (fn: Mutation | Query): string =>
   `${fn.name}${
     fn.inputs && fn.inputs.length > 0
-      ? `(${fn.inputs.map((input) => parseField(input)).join(", ")})`
-      : ""
+      ? `(${fn.inputs.map((input) => parseField(input)).join(', ')})`
+      : ''
   }: ${fn.returnType.name}!`;
 
 export const getTypes = (fieldTypes: FieldType[]): string => {
-  const types: string[] = [];
+  const types: string[] = [`type Query\ntype Mutation\n`];
   fieldTypes.forEach((field) => {
     let type = ``;
     if (!field.fields || field.fields.length === 0) return;
@@ -32,42 +32,42 @@ export const getTypes = (fieldTypes: FieldType[]): string => {
       field.fields.forEach((field) => {
         type += `${field.name} | `;
       });
-      return types.push(type.slice(0, -3) + "\n");
+      return types.push(type.slice(0, -3) + '\n');
     } else if (field.isEnum) {
       type += `enum ${field.name} {`;
       field.fields.forEach((field) => {
         type += `\n\t${field.name}`;
       });
-      return types.push(type + "\n}\n");
+      return types.push(type + '\n}\n');
     } else type += `type ${field.name} {`;
-    
+
     field.fields.forEach((field) => {
       type += `\n  ${parseField(field)}`;
     });
-    type += "\n}\n";
+    type += `\n}\n`;
 
     types.push(type);
   });
 
-  return types.join("");
+  return types.join('');
 };
 
 const getFunctions = (object: Object): string => {
-  let queries = "";
-  let mutations = "";
+  let queries = '';
+  let mutations = '';
   if (object.queries.length > 0) {
     queries += `extend type Query {`;
     object.queries.forEach((query) => {
       queries += `\n  ${parseFunction(query)}`;
     });
-    queries += "\n}\n";
+    queries += '\n}\n';
   }
   if (object.mutations.length > 0) {
     mutations += `extend type Mutation {`;
     object.mutations.forEach((mutation) => {
       mutations += `\n  ${parseFunction(mutation)}`;
     });
-    mutations += "\n}\n";
+    mutations += '\n}\n';
   }
 
   return queries + mutations;
