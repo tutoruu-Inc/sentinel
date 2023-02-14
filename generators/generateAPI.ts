1df57cc3fd3ab28df40fc2365907c064e43b9576
Youssef <ya1027@nyu.edu>
2023-02-14 18:57:30 +0200

    rset cache

--- generators/generateAPI.ts
+++ generators/generateAPI.ts
@@ -15,6 +15,7 @@ export const generateAPI = (service: Service, resolvers: string): string => {
       `\toverride willSendRequest(request: WillSendRequestOptions) {\n` +
       `\t\trequest.headers['Authorization'] = this.token ?? request.headers['Authorization'] ?? '';\n}\n\n`;
   }
+  
   api += `\t${resolvers}\n`;
   api += '}\n\n';
   api += `export const ${service.name
