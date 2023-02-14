bd433780b6e44e1f5e2994f61f06ba95be0a0022
Youssef <ya1027@nyu.edu>
2023-02-14 18:59:55 +0200

    reset cache

--- generators/generateAPI.ts
+++ generators/generateAPI.ts
@@ -2,23 +2,19 @@ import { Service } from './fetchSchema.js';
 
 export const generateAPI = (service: Service, resolvers: string): string => {
   const _class = `${service.name.replace(' ', '')}API`;
-  let api = `import { HTTPCache, RESTDataSource${
-    service.protected ? ', RequestOptions' : ''
-  } } from "apollo-datasource-rest";\n`;
+  let api = `import { RESTDataSource${
+    service.protected ? ', WillSendRequestOptions' : ''
+  } } from "@apollo/datasource-rest";\n`;
   api += `import { Resolvers } from "../../generated/types.js";\n`;
   api += `import { queryable } from "../../utils/Query.js";\n\n`;
   api += `class ${_class} extends RESTDataSource {\n`;
   api += `\toverride baseURL = "${service.baseApiUrl}";\n`;
-  api +=
-    'initialize({ context }: { context: any }) {\nthis.context = context\nthis.httpCache = new HTTPCache()\n}\n\n';
-
   if (service.protected) {
     api += `\ttoken: string = "";\n\n`;
     api +=
-      `\toverride willSendRequest(request: RequestOptions) {\n` +
-      `\t\trequest.headers.set('Authorization', this.token ?? request.headers.get('Authorization') ?? '');\n}\n\n`;
+      `\toverride willSendRequest(request: WillSendRequestOptions) {\n` +
+      `\t\trequest.headers['Authorization'] = this.token ?? request.headers['Authorization'] ?? '';\n}\n\n`;
   }
-  
   api += `\t${resolvers}\n`;
   api += '}\n\n';
   api += `export const ${service.name
